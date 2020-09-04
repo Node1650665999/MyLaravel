97a5cb86d1a144f99518f8e4020f647d64c8ca3e
THINKPAD\THINK <1650665999@qq.com>
2020-09-04 18:40:13 +0800

    init

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,11 +9,6 @@ class TestController extends Controller
 {
     public function userInfo()
     {
-        ini_set('memory_limit', 0);
-        print_r(debug_backtrace());
-//        debug_print_backtrace();
-
-        dd(123);
         $testModle = new TestModel();
         $testModle->userInfo(22);
     }
